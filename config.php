e10e962da7d0b1ec2b11517a27e2f7b8f6f5bc3f
Bram <maulanamalik6696@gmail.com>
2024-05-10 13:03:33 +0700

    update_database

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
     // konfigurasi db
     $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db   = 'extravaganza_undian';
+    $user = 'kiucoid_undian';
+    $pass = 'karismaextravaganza';
+    $db   = 'kiucoid_undian';
     
     $koneksi = mysqli_connect($host,$user,$pass,$db);
     if(!$koneksi){
         die('koneksi gagal :');
     }
-    ?>
-
-    
+    ?>  
