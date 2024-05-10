17e1542fee823178bc253ea4688c2d9a805c7548
Bram <maulanamalik6696@gmail.com>
2024-05-10 15:45:04 +0700

    update_silver7

--- save-win-silver.php
+++ save-win-silver.php
@@ -1,20 +1,14 @@
 <?php
+include 'config.php';
 
-$host = 'localhost';
-$user = 'kiucoid_undian';
-$pass = 'karismaextravaganza';
-$db   = 'kiucoid_undian';
 
 $no_undian  = $_POST['noundi'];
 $nama_toko  = $_POST['nama_toko'];
 $hadiah     = $_POST['hadiah'];
 
-$koneksi = mysqli_connect($host,$user,$pass,$db);
-if(!$koneksi){
-    die('koneksi gagal :');
-}
 
-$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,id_prize) VALUES('".$no_undian."','".$nama_toko."',1,'".$hadiah."')");
+
+$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,hadiah) VALUES('".$no_undian."','".$nama_toko."',1,'".$hadiah."')");
 if($sql){
     echo "<div style='color:green'> DATA BERHASIL DISIMPAN </div>";
 }else{
