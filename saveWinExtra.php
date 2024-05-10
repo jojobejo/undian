f3050b1d7e45f26a43991d9f47ad04927ca8dbd0
karisma_bramit <maulanamalik6696@gmail.com>
2024-05-10 20:00:57 +0700

    fix_undian_extra

--- saveWinExtra.php
+++ saveWinExtra.php
@@ -3,11 +3,11 @@ include 'config.php';
 
 $no_undian  = $_POST['noundi'];
 $nama_toko  = $_POST['nama_toko'];
+$hadiah     = $_POST['hadiah'];
 
-$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,hadiah) VALUES('".$no_undian."','".$nama_toko."',5,'16')");
+$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,hadiah) VALUES('".$no_undian."','".$nama_toko."',5,'".$hadiah."')");
 if($sql){
     echo "<div style='color:green'> DATA BERHASIL DISIMPAN </div>";
 }else{
     echo "<div style='color:red'> DATA GAGAL DISIMPAN </div>";
 }
-?>
