3f7a34baf3a6df1c77291b8e1142093b3dd6cee7
karisma_bramit <maulanamalik6696@gmail.com>
2024-05-10 20:03:57 +0700

    update_db_extra

--- saveWinExtra.php
+++ saveWinExtra.php
@@ -5,7 +5,7 @@ $no_undian  = $_POST['noundi'];
 $nama_toko  = $_POST['nama_toko'];
 $hadiah     = $_POST['hadiah'];
 
-$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,hadiah) VALUES('".$no_undian."','".$nama_toko."',5,'".$hadiah."')");
+$sql = mysqli_query($koneksi, "INSERT INTO tb_win(nomor_undian,nama_toko,kat_undian,hadiah) VALUES('".$no_undian."','".$nama_toko."',5,3)");
 if($sql){
     echo "<div style='color:green'> DATA BERHASIL DISIMPAN </div>";
 }else{
