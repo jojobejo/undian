607a429f93cfe5bf3e663b42fc2594f2d099abc5
Bram <maulanamalik6696@gmail.com>
2024-05-24 20:24:31 +0700

    update_db_domain

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
-    // DOMAIN
-    // $host = 'localhost';
-    // $user = 'kiucoid_undian';
-    // $pass = 'karismaextravaganza';
-    // $db   = 'kiucoid_undian';
+// DOMAIN
+$host = 'localhost';
+$user = 'kiucoid_undian';
+$pass = 'karismaextravaganza';
+$db   = 'kiucoid_undian';
 
-    // // LOCAL
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db   = 'kiucoid_undian';
-    
-    $koneksi = mysqli_connect($host,$user,$pass,$db);
-    if(!$koneksi){
-        die('koneksi gagal :');
-    }
+// // LOCAL
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db   = 'kiucoid_undian';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) {
+    die('koneksi gagal :');
+}
